fb74984cd93abff69fa1837d1d1ae4dd962bc257
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-09-11 14:11:37 +0200

    add a new example for the 1.0.0

---
+++ Examples/VideoBlur/index.js
@@ -0,0 +1,41 @@
+const React = require("react");
+const HueRotate = require("./HueRotate");
+const Blur = require("./Blur");
+const Field = require("./Field");
+
+class Demo extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      time: 0,
+      blur: 0,
+      blurPasses: 2,
+      hue: 0
+    };
+  }
+  componentDidMount () {
+    const loop = time => {
+      requestAnimationFrame(loop);
+      this.setState({ time: time / 1000 });
+    };
+    requestAnimationFrame(loop);
+  }
+  render () {
+    const { width, height } = this.props;
+    const { time, blur, hue, blurPasses } = this.state;
+    return <div style={{ width: width+"px" }}>
+        <Blur debug width={width} height={height} passes={blurPasses} factor={blur}>
+        <HueRotate hue={hue}>
+          <video autoPlay loop>
+            <source type="video/mp4" src="video.mp4" />
+          </video>
+        </HueRotate>
+      </Blur>
+      <Field min={0} max={2*Math.PI} value={hue} onChange={hue => this.setState({ hue })} name="Hue" width={width} />
+      <Field min={0} max={16} value={blur} onChange={blur => this.setState({ blur })} name="Blur" width={width} />
+      <Field min={2} max={8} step={1} value={blurPasses} onChange={blurPasses => this.setState({ blurPasses })} name="Blur Passes" width={width} />
+    </div>;
+  }
+}
+
+React.render(<Demo width={640} height={480} />, document.getElementById("container"));
