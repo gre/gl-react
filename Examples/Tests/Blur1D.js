f61ed34e149650f076d775554cd9de04a1baff3b
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-10-09 21:34:13 +0200

    fix Blur1D of Tests example

--- Examples/Tests/Blur1D.js
+++ Examples/Tests/Blur1D.js
@@ -9,19 +9,17 @@ const shaders = GL.Shaders.create({
 });
 
 module.exports = GL.createComponent(
-  ({ width, height, direction, minBlur, maxBlur, blurMap, offset, children }) =>
-  <GL.View
-    shader={shaders.blur1D}
-    width={width}
-    height={height}
-    uniforms={{
-      direction,
-      minBlur,
-      maxBlur,
-      blurMap,
-      offset,
-      resolution: [ width, height ]
-    }}>
-    <GL.Uniform name="t">{children}</GL.Uniform>
-  </GL.View>,
-  { displayName: "Blur1D" });
+  ({ width, height, direction, children }) =>
+    <GL.View
+      shader={shaders.blur1D}
+      width={width}
+      height={height}
+      uniforms={{
+        direction,
+        resolution: [ width, height ]
+      }}>
+      <GL.Uniform name="t">{children}</GL.Uniform>
+    </GL.View>
+, {
+  displayName: "Blur1D"
+});
