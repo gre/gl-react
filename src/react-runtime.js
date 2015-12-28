154bfb0aa37017689130baa91182a1581800c4ad
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2015-12-28 14:52:45 +0100

    Fixes #40: clarify error message for React injection

--- src/react-runtime.js
+++ src/react-runtime.js
@@ -1,7 +1,7 @@
 const runtime = require("./react-runtime-mutate").get();
 if (!runtime) {
   console.warn( // eslint-disable-line no-console
-`Please prepend in your JavaScript entry point one of following imports:
+`Please prepend in your JavaScript entry file one of following imports:
 
 ${"require"}("gl-react/react")         // for React
 
@@ -11,13 +11,13 @@ ${"require"}("gl-react/react-native")  // for React Native
 
 
 Make sure to do this BEFORE any other imports. (that are gl-react related)
-If you use any ES6 import, please use import syntax too because Babel transpile imports before requires.
+If you use any ES6 import in that file, please also use import syntax (Babel transpiles import before require syntax).
 E.g:
 import __ from "gl-react/react-native";
 
 
 > Note: This mechanism will be removed once React Native depends on React`);
 
-  throw new Error("gl-react: React instance not available at runtime. Please read instructions");
+  throw new Error(`[gl-react] React not set: Please import 'gl-react/react' OR 'gl-react/react-native' once at the beginning of your JavaScript entry file`);
 }
 module.exports = runtime;
