6cb31dc12e1ce93a5e23bc2e2dbdb16f0a9b98d8
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-05-29 17:52:23 +0200

    also use gl-transitions for the RN cookbook

--- packages/cookbook-rn-shared/src/examples/transitions/index.js
+++ packages/cookbook-rn-shared/src/examples/transitions/index.js
@@ -1,60 +1,31 @@
-import React, { Component } from "react";
-import { Shaders, Node, LinearCopy, connectSize } from "gl-react";
-import getGLReactImplementation from "../../gl-react-implementation"; const { Surface } = getGLReactImplementation();
+import React from "react";
+import { LinearCopy } from "gl-react";
+import GLTransition from "react-gl-transition";
+import GLTransitions from "gl-transitions";
 import timeLoop from "../../HOC/timeLoop";
-import {shadersDefs, randomTransition} from "./transitions";
-const shaders = Shaders.create(shadersDefs);
-
-const Transition = connectSize(
-({ shader, progress, from, to, uniforms, width, height }) =>
-  <Node
-    shader={shader}
-    uniforms={{ progress, from, to, ...uniforms, resolution: [width,height] }}
-  />);
-
-class Slideshow extends Component {
-  state = {
-    index: 0,
-    transition: randomTransition(),
-  };
-  componentWillReceiveProps ({ time, slideDuration, transitionDuration }) {
-    const index = Math.floor(time / (slideDuration + transitionDuration));
-    if (this.state.index !== index) {
-      this.setState({
-        index,
-        transition: randomTransition(),
-      });
-    }
-  }
-  render() {
-    const { slides, slideDuration, transitionDuration, time } = this.props;
-    const { index, transition } = this.state;
-    const totalDuration = slideDuration + transitionDuration;
-    const progress = Math.max(0,
-      (time - index * totalDuration - slideDuration) / transitionDuration);
-    const from = slides[index % slides.length];
-    const to = slides[(index+1) % slides.length];
-    return (
-      progress
-      ? <Transition
-          from={from}
-          to={to}
-          progress={progress}
-          shader={shaders[transition.name]}
-          uniforms={transition.uniforms}
-        />
-      : <LinearCopy>{from}</LinearCopy>
-    );
-  }
-}
-const SlideshowLoop = timeLoop(Slideshow);
-
 import images from "./images";
-export default ({ width }) =>
+import getGLReactImplementation from "../../gl-react-implementation";
+const { Surface } = getGLReactImplementation();
+
+const Slideshow = timeLoop(({ slides, delay, duration, time }) => {
+  const index = Math.floor(time / (delay + duration));
+  const from = slides[index % slides.length];
+  const to = slides[(index + 1) % slides.length];
+  const transition = GLTransitions[index % GLTransitions.length];
+  const total = delay + duration;
+  const progress = (time - index * total - delay) / duration;
+  return progress > 0
+    ? <GLTransition
+        from={from}
+        to={to}
+        progress={progress}
+        transition={transition}
+      />
+    : <LinearCopy>{from}</LinearCopy>;
+});
+
+export default ({ width }) => (
   <Surface style={{ width, height: Math.round(width * 0.6) }}>
-    <SlideshowLoop
-      slides={images}
-      slideDuration={2000}
-      transitionDuration={1500}
-    />
+    <Slideshow slides={images} delay={2000} duration={1500} />
   </Surface>
+);
