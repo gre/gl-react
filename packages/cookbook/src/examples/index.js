a681adf4e32a43dee6a595b1e57c1874f5c514af
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-07-05 12:20:37 +0200

    fix cookbook to show the toolbox

--- packages/cookbook/src/examples/index.js
+++ packages/cookbook/src/examples/index.js
@@ -45,7 +45,11 @@ ibex`.split("\n");
 const examples = {};
 
 names.forEach(name => {
-  const { default: Example, ...rest } = require("./" + name + "/index.js");
+  const { default: Example } = require("./" + name + "/index.js");
+  const {
+    default: _, // eslint-disable-line
+    ...rest
+  } = require("./" + name + "/meta.js");
   const source = require("./" + name + "/index.source.js");
   examples[name] = { ...rest, Example, source };
 });
