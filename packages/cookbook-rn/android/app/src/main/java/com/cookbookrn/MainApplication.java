4e66168e5e6d93d2c801578ac4ceff87e5f9985c
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-09-01 18:54:30 +0200

    fix cookbook-rn/android

--- packages/cookbook-rn/android/app/src/main/java/com/cookbookrn/MainApplication.java
+++ packages/cookbook-rn/android/app/src/main/java/com/cookbookrn/MainApplication.java
@@ -13,7 +13,7 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-import fr.greweb.rngl.RNGLPackage;
+import fr.greweb.rnwebgl.RNWebGLPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RNGLPackage()
+          new RNWebGLPackage()
       );
     }
   };
