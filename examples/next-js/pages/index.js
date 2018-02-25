c8a6d00e29d4605842304b86a3a6f9ee8d2aaa72
Gaëtan Renaudeau <gaetan.renaudeau@ledger.fr>
2018-02-25 10:09:01 +0100

    next-js example (just to check it works)

---
+++ examples/next-js/pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Shaders, Node, GLSL } from "gl-react";
+import { Surface } from "gl-react-dom";
+
+const shaders = Shaders.create({
+  helloBlue: {
+    frag: GLSL`
+precision highp float;
+varying vec2 uv;
+uniform float blue;
+void main() {
+  gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
+}`
+  }
+});
+class HelloBlue extends React.Component {
+  render() {
+    const { blue } = this.props;
+    return <Node shader={shaders.helloBlue} uniforms={{ blue }} />;
+  }
+}
+
+export default () => (
+  <Surface width={300} height={300}>
+    <HelloBlue blue={0.5} />
+  </Surface>
+);
