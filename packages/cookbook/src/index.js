379b66c9c0aa2bd26dfc8000557a259e23695f96
tuanaiseo <tuanaiseo@gmail.com>
2026-04-05 18:03:36 +0700

    fix(security)(cookbook): insecure dev-script injection via http and documen
    The app conditionally injects a script using `document.write('<script src="http://localhost:8097"></script>')` when `NODE_ENV !== 'production'`. If this code runs in any non-production deployment (including misconfigured staging), it trusts an unauthenticated HTTP endpoint and executes arbitrary JavaScript from it, enabling code execution in the browser.
    
    Affected files: index.js
    
    Signed-off-by: tuanaiseo <221258316+tuanaiseo@users.noreply.github.com>

--- packages/cookbook/src/index.js
+++ packages/cookbook/src/index.js
@@ -4,9 +4,6 @@ import { render } from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 import App from "./App";
 
-if (process.env.NODE_ENV !== "production") {
-  document.write('<script src="http://localhost:8097"></script>'); // Nuclide React Inspector
-}
 
 render(
   <BrowserRouter>
